736aa0236a48a4182852cc48f2f4fcba72557dac
Dom B <domlamont@yahoo.com>
2021-11-16 17:35:27 -0800

    Created Robot Class with Constructor

---
+++ robotClasses/robot.js
@@ -0,0 +1,15 @@
+
+class Robot {
+    /* Robot to have a name, health, power level, 
+    and a Weapon with a type (i.e. sword) and attack power */
+
+    constructor(){
+        this.name = "robot";
+        this.health = 100;
+        this.powerLevel = 100;
+        this.weapon = "sword";
+        this.attackPower = 10;
+    }
+}
+
+module.exports = Robot;
