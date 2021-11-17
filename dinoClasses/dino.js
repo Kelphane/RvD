2e63fc839cf714edc9b167223c46df1c1d2cfc8b
Dom B <domlamont@yahoo.com>
2021-11-16 17:30:11 -0800

    Initial Commit

---
+++ dinoClasses/dino.js
@@ -0,0 +1,14 @@
+
+class Dino {
+    /* Dinosaur to have a type, health, energy, and 
+    attack power. */
+
+    constructor(){
+        this.type = "Dino";
+        this.health = 100;
+        this.energy = 100;
+        this.attackPower = 10;
+    }
+}
+
+module.exports = Dino;
